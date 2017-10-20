da39999aaeed17a63025f796defb2725f7afb8d0
Salva Corts <salvacorts97@gmail.com>
2017-10-21 00:13:24 +0200

    Identation

--- fib.go
+++ fib.go
@@ -9,14 +9,14 @@ import (
 
 func Fib(n int) int {
   if n == 0 || n == 1 {
-		return n
-	}
+    return n
+  }
 
   fibMem := make([]int, n+1);
   fibMem[0] = 0;
   fibMem[1] = 1;
 
-	for i := 2; i <= n; i++ {
+  for i := 2; i <= n; i++ {
     fibMem[i] = fibMem[i-1] + fibMem[i-2];
   }
 
